0d2bd96e6d78a414457623115085d841775b992e
hayden-siok13 <hayden.siok@mymail.champlain.edu>
2025-02-19 12:56:26 -0500

    secure ssh

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,8 @@
+#secure-ssh.sh
+#author hayden-siok13
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+
+#Check if a username is provided
+echo "ALL OF YOUR CODE GOES HERE"
